c11e041edfc4dbfbbfa020cb1bd389c634cb11c9
Your Name <jcardo47@eafit.edu.co>
2011-10-16 23:52:06 -0500

    Some changes done

--- RFPs/src/com/rfp/manager/UserManager.java
+++ RFPs/src/com/rfp/manager/UserManager.java
@@ -1,5 +1,6 @@
 package com.rfp.manager;
 
+import com.rfp.exception.UserRegisterException;
 import com.rfp.service.UserService;
 import com.rfp.to.UserTO;
 
@@ -9,5 +10,19 @@ public class UserManager
 	{
 		UserService service = new UserService();
 		return service.login(user);
+	}
+	
+	public UserTO registerUser(UserTO userTO) throws UserRegisterException
+	{
+		UserService service = new UserService();
+		if (service.userNameExist(userTO))
+		{
+			throw new UserRegisterException();
+		}
+		else
+		{
+			return service.registerUser(userTO);
+		}
+		
 	}
 }
--- RFPs/src/com/rfp/wrapper/RFPWrapper.java
+++ RFPs/src/com/rfp/wrapper/RFPWrapper.java
@@ -1,5 +1,6 @@
 package com.rfp.wrapper;
 
+import com.rfp.exception.UserRegisterException;
 import com.rfp.manager.RequestManager;
 import com.rfp.manager.UserManager;
 import com.rfp.to.RequestTO;
@@ -18,4 +19,11 @@ public class RFPWrapper
 		RequestManager manager = new RequestManager();
 		manager.registerRFP(request);
 	}
+	
+	public UserTO registerUser(UserTO userTO) throws UserRegisterException
+	{
+		UserManager manager = new UserManager(); 
+		return manager.registerUser(userTO);
+	}
+	
 }
