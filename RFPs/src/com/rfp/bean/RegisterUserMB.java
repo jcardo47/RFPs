03b83fabd0ef41cc0e44054a1b69c403174cde96
jcardo47 <djjhmesa@hotmail.com>
2011-10-23 22:58:54 -0500

    Some changes in Register User

--- RFPs/src/com/rfp/bean/RegisterUserMB.java
+++ RFPs/src/com/rfp/bean/RegisterUserMB.java
@@ -19,7 +19,7 @@ public class RegisterUserMB
 	private String message;
 	private String email2;
 	private String butLabel = "Registrar";
-	private Boolean renderPass = true;
+	private Boolean renderPass = false;
 	
 	public RegisterUserMB ()
 	{
@@ -35,7 +35,7 @@ public class RegisterUserMB
 		if (userTo != null)
 		{
 			butLabel = "Guardar";
-			renderPass = false;
+			renderPass = true;
 			fillTexts(userTo);
 			
 		}
